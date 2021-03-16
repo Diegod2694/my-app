2bf642d98d3379ccb21d1e0807455b062fe188bd
Diego Parra <diegod2694@gmail.com>
2021-03-16 19:39:23 -0400

    First commit

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Oil Change App</Text>
+      <TextInput />
+      <TextInput />
+      <Button
+        onPress={() => console.log('navegue')}
+        title="Continuar"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+      />
       <StatusBar style="auto" />
     </View>
   );
